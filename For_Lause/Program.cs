e0e6fd7fb646410e659a87dbfea06eb558174552
Ruben Galoyan <rugalo@ttu.ee>
2019-02-02 16:03:04 +0200

    Created solution and first project

---
+++ For_Lause/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace For_Lause
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

608b830bc503e639052dfaedad0e05a5c6b7218b
Ruben Galoyan <rugalo@ttu.ee>
2019-02-02 21:23:11 +0200

    Finished working with collections (ArrayList, List, Dictionary, Array). Object and collection initializers.

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,84 @@ namespace WorkingWithCollections
     {
         static void Main(string[] args)
         {
+            /*
+            Car car1 = new Car();
+            car1.Make = "Oldsmobile";
+            car1.Model = "Cutlas Supreme";
+            car1.VIN = "A1";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+            car2.VIN = "A2";
+
+            Book book1 = new Book();
+            book1.Author = "Robert Tabor";
+            book1.Title = "Microsoft .NET XML Web Services";
+            book1.ISBN = "0-000-00000-0";
+            */
+
+            /*
+            Old collection style, convenient, but may lead to error because of no restrictions on data types
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(car1);
+            arrayList.Add(car2);
+            arrayList.Add(book1);
+            arrayList.Remove(book1);
+
+            foreach (Car car in myArrayList)
+            {
+                Console.WriteLine(car.Make);
+            }
+            */
+            
+            /*
+            List<Car> myList = new List<Car>();
+            myList.Add(car1);
+            myList.Add(car2);
+            //myList.Add(b1); <- wont work, because this is a list of Cars, cannot add Books
+            foreach (Car car in myList)
+            {
+                Console.WriteLine(car.Model);
+            }
+            */
+
+            /*
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+
+            myDictionary.Add(car1.VIN, car1);
+            myDictionary.Add(car2.VIN, car2);
+
+            Console.WriteLine(myDictionary["A2"].Make);
+            */
+
+            string[] names = { "Bob", "Brian", "Steve", "Chuck" };
+
+            //object initializer
+            Car car1 = new Car() { Make = "BMW", Model = "750li", VIN = "C3" };
+            Car car2 = new Car() { Make = "Toyota", Model = "4Runner", VIN = "D4" };
+
+            //Collection initializer
+            List<Car> myList = new List<Car>()
+            {
+                new Car { Make = "Oldsmobile", Model = "Cutlas Supreme", VIN = "E5" },
+                new Car { Make = "Nissan", Model = "Altima", VIN = "F6" }
+            };
+
+            Console.ReadLine();
         }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string VIN { get; set; }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }

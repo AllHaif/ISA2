5f8428c8b3c89b171899eaf44f5db48ed0060151
Ruben Galoyan <rugalo@ttu.ee>
2019-02-02 20:24:50 +0200

    Created a library and added it as a refernce to another project

---
+++ CodeLibrary/Scrape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapeWebPage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, url);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
---
+++ MyClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeLibrary;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape scrape = new Scrape();
+            string value = scrape.ScrapeWebPage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}

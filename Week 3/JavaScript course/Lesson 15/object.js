bf196d51be0c72602a04b62568f73bc36c7ffe95
Ruben Galoyan <rugalo@ttu.ee>
2019-02-15 16:34:13 +0200

    Lesson 15

---
+++ Week 3/JavaScript course/Lesson 15/object.js
@@ -0,0 +1,53 @@
+
+let car = {
+    make: 'BMW',
+    model: 'M4 CS',
+    year: 2019,
+    getPrice: function() {
+        return 5000;
+    },
+    printDescription: function() {
+        console.log(this.make + ' ' + this.model);
+    }
+}
+
+car.printDescription();
+console.log(car.year);
+console.log(car['year']);
+console.log(car[1]);
+
+
+var anotherCar = {};
+anotherCar.whatever = 'bob';
+console.log(anotherCar.whatever);
+
+var a = {
+    myProperty: { b: 'hi' }
+};
+console.log(a.myProperty.b);
+
+var c = {
+    myProperty: [
+        { d: 'this' },
+        { e: 'can' },
+        { f: 'get' },
+        { g: 'crazy' }
+    ]
+};
+
+let carLot = [
+    { year: 2017, make: 'Toyota', model: '4Runner' },
+    { year: 2015, make: 'BMW', model: '528i' },
+    { year: 1982, make: 'Buick', model: 'Skylark' },
+];
+
+let contacts = {
+    customers: [
+        { firstName: 'Bob', lastName: 'Tabor', phoneNumbers: [ '(123) 456-7890', '(123) 567-8901' ] },
+        { firstName: 'Richard', lastName: 'Boughton', phoneNumbers: [ '(123) 555-7894', '(123) 555-8903' ] }
+    ],
+    employees: [
+        { firstName: 'Steve', lastName: 'Jaworski', phoneNumbers: [ '(312) 456-7890', '(312) 567-8901' ] },
+        { firstName: 'Grant', lastName: 'Tabor', phoneNumbers: [ '(312) 456-7890' ] },
+    ]
+};

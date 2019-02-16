d5147b0e5d6a91ed7e5bdf58dae3d1fba11b5254
Ruben Galoyan <rugalo@ttu.ee>
2019-02-16 18:17:49 +0200

    Lesson 18 part 2

--- Week 3/JavaScript course/Lesson 18/this.js
+++ Week 3/JavaScript course/Lesson 18/this.js
@@ -1,9 +1,8 @@
-var car = {
+/* var car = {
     make: "bmw",
     model: "745li",
     year: 2010,
     getPrice: function () {
-      // perform some calculation
       return 5000;
     },
     printDescription: function() {
@@ -18,3 +17,30 @@ function first() {
 }
 
 console.log(first() === global);
+
+function second() {
+    'use strict';
+
+    return this;
+}
+
+console.log(second() === global);
+console.log(second() === undefined); */
+
+let myObject = { value: 'My Object' };
+global.value = 'Global object';
+
+function third(name) {
+    return this.value + name;
+}
+
+//console.log(third());
+
+/* Both call and apply allow you to explicitly set
+what you want to represent 'this'.  The difference
+is in how additional arguments to the function
+are sent. */
+
+console.log(third.call(myObject, 'bob'));
+console.log(third.apply(myObject, ['bob']));
+

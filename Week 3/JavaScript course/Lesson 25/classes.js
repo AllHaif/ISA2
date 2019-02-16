c9ea6f3643590bb4a5c60c9cbf70b8766675b24a
Ruben Galoyan <rugalo@ttu.ee>
2019-02-16 22:52:30 +0200

    Lesson 25

---
+++ Week 3/JavaScript course/Lesson 25/classes.js
@@ -0,0 +1,24 @@
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    print() {
+        console.log(`${this.make} ${this.model} (${this.year})`);
+    }
+}
+
+let myCar = new Car('bmw', '745li', 2010);
+myCar.print();
+
+class SportsCar extends Car {
+    revEngine() {
+        console.log('Vrrroom goes the ' + this.model);
+    }
+}
+
+let mySportsCar = new SportsCar('dodge', 'viper', 2011);
+mySportsCar.print();
+mySportsCar.revEngine();

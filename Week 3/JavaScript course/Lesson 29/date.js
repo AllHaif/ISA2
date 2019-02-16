7ca6e869018692fd78c030f6d76e115754873175
Ruben Galoyan <rugalo@ttu.ee>
2019-02-16 23:21:03 +0200

    Lesson 29

---
+++ Week 3/JavaScript course/Lesson 29/date.js
@@ -0,0 +1,22 @@
+let today = new Date();
+
+let ruben = new Date('October 7, 1999 18:14:23');
+// let ruben = new Date('1999-10-07T18:14:23');
+// let ruben = new Date(1999, 10, 7);
+// let ruben = new Date(1999, 10, 7, 18, 14, 23);
+
+var elapsedTime = today - ruben; // in milliseconds
+console.log(elapsedTime);
+
+console.log(bob.getDate()); // Monday = 1, Sunday = 7
+console.log(bob.getTime());
+
+/*
+getMonth()
+getDay()
+
+getHours()
+getMinutes()
+getSeconds()
+getMilliseconds()
+*/

226a9934b5ed975007cd2ec696f6d22c8e2238dd
Ruben Galoyan <rugalo@ttu.ee>
2019-02-16 23:04:18 +0200

    Lesson 26

---
+++ Week 3/JavaScript course/Lesson 26/arrow-functions.js
@@ -0,0 +1,17 @@
+let hi = () => {console.log('howdy');}
+hi();
+
+let hi = (name) => {console.log(`howdy ${name}`)};
+hi('bob');
+
+let add = (a, b) => { return a + b };
+console.log(add(7, 3));
+
+let names = ['david', 'eddie', 'alex', 'michael'];
+names.map((name) => {console.log(`howdy ${name}!`)});
+
+let i = 0;
+names.map((name) => {i++; console.log(`howdy ${name} ${i}!`);});
+
+var tranformed = names.map((name) => { return `howdy ${name}!` });
+console.log(tranformed);

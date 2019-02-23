62580d85f3925c99f7b993bcd5780ad1f447da9a
Ruben Galoyan <rugalo@ttu.ee>
2019-02-23 16:30:12 +0200

    Labor 6

--- Week 4/ASP.NET MVC/Controllers/TestController.cs
+++ Week 4/ASP.NET MVC/Controllers/TestController.cs
@@ -2,25 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ASP.NET_MVC.Models;
+using Facade;
+using Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_MVC.Controllers
 {
     public class TestController : Controller
     {
-        public string GetString()
-        {
-            return "Hello world!";
-        }
-
         public ActionResult GetView()
         {
-            Employee emp = new Employee();
-            emp.FirstName = "Ruben";
-            emp.LastName = "Galoyan";
-            emp.Salary = 50000;
-            return View("MyView", emp);
+            var emp = new Employee("Ruben", "Galoyan", 20000);
+
+            var vmEmp = new EmployeeViewModel(emp,"Admin");
+            return View("MyView", vmEmp);
         }
     }
 }

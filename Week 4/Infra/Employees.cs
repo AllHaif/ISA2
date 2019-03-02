64815fe43c8e28e6e64d28330187af34f968cbfa
Ruben Galoyan <ruben071099@gmail.com>
2019-03-02 20:40:22 +0200

    Labor 26

--- Week 4/Infra/Employees.cs
+++ Week 4/Infra/Employees.cs
@@ -27,5 +27,10 @@ namespace Infra
             if (user.UserName == "Mari" && user.Password == "Mets") return true;
             return false;
         }
+        public void UploadEmployees(List<Employee> employees, SalesDbContext db)
+        {
+            db.Employees.AddRange(employees);
+            db.SaveChanges();
+        }
     }
 }

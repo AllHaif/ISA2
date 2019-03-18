8087bb693a5556aa6caf255a4c7b46f5b974080a
Ruben Galoyan <ruben071099@gmail.com>
2019-03-18 23:12:09 +0200

    Razor Pages 2.6 - Read Related Data

---
+++ Week 7/ContosoUniversity_2/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity_2.Models;
+
+namespace ContosoUniversity_2.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity_2.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity_2.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
